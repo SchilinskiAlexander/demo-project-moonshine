809083e482ba42d383fcab92cfac3e5d8bb24ef7
levchenko-ivan <ivan97111@yandex.ru>
2024-11-03 14:43:16 +0300

    feat: metrics

--- app/MoonShine/Pages/Dashboard.php
+++ app/MoonShine/Pages/Dashboard.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Pages;
 
+use App\Models\Article;
+use App\Models\Comment;
+use MoonShine\Apexcharts\Components\DonutChartMetric;
+use MoonShine\Apexcharts\Components\LineChartMetric;
 use MoonShine\Laravel\Pages\Page;
-use MoonShine\Contracts\UI\ComponentContract;
+use MoonShine\UI\Components\Layout\Column;
+use MoonShine\UI\Components\Layout\Grid;
+use MoonShine\UI\Components\Layout\LineBreak;
+use MoonShine\UI\Components\Metrics\Wrapped\ValueMetric;
+
 
 class Dashboard extends Page
 {
-    /**
-     * @return array<string, string>
-     */
     public function getBreadcrumbs(): array
     {
         return [
@@ -21,14 +26,52 @@ class Dashboard extends Page
 
     public function getTitle(): string
     {
-        return $this->title ?: 'Dashboard';
+        return 'Dashboard';
     }
 
-    /**
-     * @return list<ComponentContract>
-     */
-    protected function components(): iterable
+    public function components(): array
 	{
-		return [];
+		return [
+//            TextBlock::make(
+//                'Welcome to MoonShine!',
+//                'Demo version'
+//            ),
+
+            LineBreak::make(),
+
+            Grid::make([
+                Column::make([
+                    ValueMetric::make('Articles')
+                        ->value(Article::query()->count()),
+                ])->columnSpan(6),
+
+                Column::make([
+                    ValueMetric::make('Comments')
+                        ->value(Comment::query()->count()),
+                ])->columnSpan(6),
+
+                Column::make([
+                    DonutChartMetric::make('Подписчики')
+                        ->columnSpan(6)
+                        ->values(['CutCode' => 10000, 'Apple' => 9999]),
+                ])->columnSpan(6),
+
+                Column::make([
+                    LineChartMetric::make('Заказы')
+                        ->line([
+                            'Выручка 1' => [
+                                now()->format('Y-m-d') => 100,
+                                now()->addDay()->format('Y-m-d') => 200
+                            ]
+                        ])
+                        ->line([
+                            'Выручка 2' => [
+                                now()->format('Y-m-d') => 300,
+                                now()->addDay()->format('Y-m-d') => 400
+                            ]
+                        ], '#EC4176'),
+                ])->columnSpan(6)
+            ]),
+        ];
 	}
 }
